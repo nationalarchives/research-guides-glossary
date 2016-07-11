8e90276e2c171b452819ff81238797d725985ff2
Andy Aldridge <andy@grange85.co.uk>
2016-07-11 15:09:17 +0100

    fix paths

--- research-guide-glossary.php
+++ research-guide-glossary.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * Plugin Name: TNA research guides glossary
- * Plugin URI: https://github.com/nationalarchives/tna-research-guides-glossary
- * Description: Display the current research guides glossary
+ * Plugin Name: TNA research guide glossary
+ * Plugin URI: https://github.com/nationalarchives/tna-research-guide-glossary
+ * Description: Display the current research guide glossary
  * Version: 0.1
  * Author: Andy Aldridge (andrew.aldridge@nationalarchives.gsi.gov.uk)
  * Author URI: https://github.com/nationalarchives/
  * License: GPL2
  */
 
-add_action('admin_menu', 'tna_research_guides_glossary_settings');
-function tna_research_guides_glossary_settings()
+add_action('admin_menu', 'tna_research_guide_glossary_settings');
+function tna_research_guide_glossary_settings()
 {
-    add_options_page('Research guides glossary', 'Research guides glossary', 'edit_pages', 'rg-glossary',
-        'tna_research_guides_glossary_settings_page');
+    add_options_page('Research guide glossary', 'Research guide glossary', 'edit_pages', 'rg-glossary',
+        'tna_research_guide_glossary_settings_page');
 }
 
-function tna_research_guides_glossary_settings_page()
+function tna_research_guide_glossary_settings_page()
 {
     // admin
 	require_once('research-guide-glossary-data.php');
     ?>
     <div class="wrap">
-        <h2>Research guides glossary</h2>
+        <h2>Research guide glossary</h2>
 
 		<?php
 		foreach ($glossaryDefinitions as $term => $definition) {
